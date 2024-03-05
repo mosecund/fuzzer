c784e3667ca17c9ac72b491df1cea16e48214b04
Ismael Secundar <73141122+mosecund@users.noreply.github.com>
2024-03-05 19:57:19 +0100

    mode ending_bytes size
    tests dones

--- src/fuzz.c
+++ src/fuzz.c
@@ -55,13 +55,12 @@ int test_extraction(char* extractor_path) {
   ctr_test++;
 
   int rv = 0;
-  char cmd[256];
+  char cmd[84];
   strncpy(cmd, extractor_path, 58);
   cmd[59] = '\0';
   strncat(cmd, " archive.tar", 25);
   char buf[33];
   FILE *fp;
-//  fp = popen(cmd, "r");
 
   if ((fp = popen(cmd, "r")) == NULL) {
     printf("Error opening pipe!\n");
@@ -78,6 +77,12 @@ int test_extraction(char* extractor_path) {
   } else {
     printf("Crash message\n");
     rv = 1;
+    // Rename file on success
+    char success_name[100];
+    snprintf(success_name, 100, "success_archive%d.tar", ctr_success);
+    rename(ARCHIVE_NAME, success_name);
+    // Success counter increased by 1
+    ctr_success++;
     goto finally;
   }
   finally:
@@ -206,47 +211,63 @@ void basic_test(char* extractor_path, char* field_name, int size, int checksum) 
   initialize_header(&header);
 
 //   Non ASCII - Test with 3 different variations
-//  update_header_field(field_name, &NOT_ASCII_CHARS[0], size);
-//  make_tar_empty(&header, checksum);
-//  test_extraction(extractor_path);
-//
-//  // Reset header
-//  initialize_header(&header);
-//
-//  update_header_field(field_name, &NOT_ASCII_CHARS[2],size );
-//  make_tar_empty(&header, checksum);
-//  test_extraction(extractor_path);
-//
-//  // Reset header
-//  initialize_header(&header);
-//
-//  update_header_field(field_name, &NOT_ASCII_CHARS[3],size );
-//  make_tar_empty(&header, checksum);
-//  test_extraction(extractor_path);
+  update_header_field(field_name, &NOT_ASCII_CHARS[0], size);
+  make_tar_empty(&header, checksum);
+  test_extraction(extractor_path);
+
+  // Reset header
+  initialize_header(&header);
+
+  update_header_field(field_name, &NOT_ASCII_CHARS[2],size );
+  make_tar_empty(&header, checksum);
+  test_extraction(extractor_path);
+
+  // Reset header
+  initialize_header(&header);
+
+  update_header_field(field_name, &NOT_ASCII_CHARS[3],size );
+  make_tar_empty(&header, checksum);
+  test_extraction(extractor_path);
 
 }
-void iterate_over_all_modes(){
-  char modes[8];
-  size_t size_modes = 8;
+//void iterate_over_all_modes(){
+//  char modes[8];
+//  size_t size_modes = 8;
+//  for (int i = 0; i < 12; i++)
+//  {
+//    initialize_header(&header);
+//    snprintf(modes, 8, "%07o", MODES[i]);//TODO voir ce que signifie le 07o
+//    update_header_field(header.mode, modes,size_modes);
+//    make_tar_empty(&header, 1);
+//    test_extraction(extractorPath);
+//  }
+//}
+//void test_mode(char* extractor_path) {
+//  printf("\nFUZZING ON MODE\n");
+//
+//  // Test with different sizes
+//  basic_test(extractor_path, header.mode, modes_size, 1);
+//  iterate_over_all_modes();
+//
+//}
+void test_mode(char* extractor_path) {
+
+  printf("\nFUZZING ON MODE FIELD\n");
+  basic_test(extractor_path, header.mode, 8, 1);
+
+  char test_mode[8];
+
+  // We can test every possible mode in the range of known modes
   for (int i = 0; i < 12; i++)
   {
     initialize_header(&header);
-    snprintf(modes, 8, "%07o", MODES[i]);//TODO voir ce que signifie le 07o
-    update_header_field(header.mode, modes,size_modes);
+    snprintf(test_mode, 8, "%07o", MODES[i]);
+    update_header_field(header.mode, test_mode, 8);
     make_tar_empty(&header, 1);
-    test_extraction(extractorPath);
+    test_extraction(extractor_path);
   }
 }
-void test_mode(char* extractor_path) {
-  printf("\nFUZZING ON MODE\n");
 
-  // Test with different sizes
-  basic_test(extractor_path, header.mode, modes_size, 1);
-  iterate_over_all_modes();
-
-}
-void iterate_over_all_size(){
-}
 void test_size(char* extractor_path) {
 
   printf("\nFUZZING ON SIZE FIELD\n");
@@ -255,11 +276,11 @@ void test_size(char* extractor_path) {
 
 
   // Test with a file with content = "Hello World!"
-  char content[] = "Testing the size field!";
+  char content[] = "Testing size field";
   size_t content_size = strlen(content);
 
   // Test with different sizes similar to the endbytes sizes
-  int content_sizes[] = {-1, 0, 1, 23, 20, 100, NUMBER_END_BYTES};
+  int content_sizes[] = {-1, 0, 1, 18, 20, 100, NUMBER_END_BYTES};
 
   // Reset header
   initialize_header(&header);
@@ -285,16 +306,17 @@ void fuzz(char *path_of_extractor){
 
     // Test 1: Create an empty tar file
 //    printf("Test %d: Create an empty tar file\n", ctr_test++);
-  initialize_header(&header);
-  make_tar_empty(&header, 1);
-    test_extraction(path_of_extractor);
-    test_ending_bytes(path_of_extractor);
-    test_mode(path_of_extractor);
-    test_size(path_of_extractor);
+//  initialize_header(&header);
+//  make_tar_empty(&header, 1);
+//    test_extraction(path_of_extractor);
+  test_mode(path_of_extractor);
+  test_ending_bytes(path_of_extractor);
+  test_size(path_of_extractor);
 
   printf("Number of tests completed: %d\n", ctr_test);
   printf("Number of crashes found: %d\n", ctr_success);
-//  printf("-------------------------- cleaning --------------\n");
-//    clean();
+//  system("ls -all *.txt");
+  printf("-------------------------- cleaning --------------\n");
+    clean();
 
 }
